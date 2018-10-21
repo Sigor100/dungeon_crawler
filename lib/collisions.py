82100db58ba5e6465fedb5cb0106e6225a2572aa
Piotr Fila <piofila2000@gmail.com>
2018-10-21 14:24:18 +0200

    collision fix

--- lib/collisions.py
+++ lib/collisions.py
@@ -8,26 +8,10 @@ hero_id = 5
 acc_block = 4
 
 def collisions(x, y, d):
-	if d == 0:
-		if generation.collidable(whatobject(x, y - box_size)):
-			return True
-		else:
-			return False
-	elif d == 1:
-		if generation.collidable(whatobject(x + box_size, y)):
-			return True
-		else:
-			return False
-	elif d == 2:
-		if generation.collidable(whatobject(x, y + box_size)):
-			return True
-		else:
-			return False
-	elif d == 3:
-		if generation.collidable(whatobject(x - box_size, y)):
-			return True
-		else:
-			return False
+	if generation.collidable(whatobject(x + d[1], y + d[0])):
+		return True
+	else:
+		return False
 
 def putonmap(x, y):
 	global acc_block
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def gameloop():
 
 	game_exit = False
 	game_over = False
-	direction = 0  # 0 = up 1 = right  2= down 	3= left
+	direction = [0, 0]  # 0 - y axis; 1 - z axis
 	hero_x = map.startpos[0]
 	hero_y = map.startpos[1]
 
@@ -27,7 +27,7 @@ def gameloop():
 	hero_y = 32 * map.startpos[1]
 	hero_x_change = 0
 	hero_y_change = 0
-	whileinput = 0
+#
 
 	while not game_exit:
 		while game_over:
@@ -40,51 +40,27 @@ def gameloop():
 			if event.type == pygame.QUIT:
 				game_exit = True
 			if event.type == pygame.KEYDOWN:
-				whileinput = whileinput + 1
-				if whileinput < 2:
-					if event.key == pygame.K_a and c.collisions(hero_x, hero_y, 3):
-						hero_x_change = -box_size
-						direction = 3
-					if event.key == pygame.K_d and c.collisions(hero_x, hero_y, 1):
-						hero_x_change = box_size
-						direction = 1
-					if event.key == pygame.K_w and c.collisions(hero_x, hero_y, 0):
-						hero_y_change = -box_size
-						direction = 0
-					if event.key == pygame.K_s and c.collisions(hero_x, hero_y, 2):
-						hero_y_change = box_size
-						direction = 2
-				else:
-					if event.key == pygame.K_a and c.collisions(hero_x, hero_y, 3) and c.collisions(hero_x, hero_y,
-																									0) and c.collisions(
-							hero_x, hero_y, 2):
-						hero_x_change = -box_size
-						direction = 3
-					if event.key == pygame.K_d and c.collisions(hero_x, hero_y, 1) and c.collisions(hero_x, hero_y,
-																									0) and c.collisions(
-							hero_x, hero_y, 2):
-						hero_x_change = box_size
-						direction = 1
-					if event.key == pygame.K_w and c.collisions(hero_x, hero_y, 0) and c.collisions(hero_x, hero_y,
-																									3) and c.collisions(
-							hero_x, hero_y, 1):
-						hero_y_change = -box_size
-						direction = 0
-					if event.key == pygame.K_s and c.collisions(hero_x, hero_y, 2) and c.collisions(hero_x, hero_y,
-																									3) and c.collisions(
-							hero_x, hero_y, 1):
-						hero_y_change = box_size
-						direction = 2
+				if event.key == pygame.K_w:
+					direction[0] -= 32
+				if event.key == pygame.K_s:
+					direction[0] += 32
+				if event.key == pygame.K_a:
+					direction[1] -= 32
+				if event.key == pygame.K_d:
+					direction[1] += 32
 			elif event.type == pygame.KEYUP:
-				whileinput = whileinput - 1
-				if event.key == pygame.K_a or event.key == pygame.K_d:
-					hero_x_change = 0
-				if event.key == pygame.K_w or event.key == pygame.K_s:
-					hero_y_change = 0
+				if event.key == pygame.K_w:
+					direction[0] += 32
+				if event.key == pygame.K_s:
+					direction[0] -= 32
+				if event.key == pygame.K_a:
+					direction[1] += 32
+				if event.key == pygame.K_d:
+					direction[1] -= 32
 		c.takeoffmap(hero_x, hero_y)
 		if c.collisions(hero_x, hero_y, direction):
-			hero_x = hero_x + hero_x_change
-			hero_y = hero_y + hero_y_change
+			hero_x = hero_x + direction[1]
+			hero_y = hero_y + direction[0]
 		else:
 			print("kolizja")
 
