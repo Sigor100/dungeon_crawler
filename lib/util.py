401b9be7fccdaebdda69d45bb9a8ea7d5a56814c
Piotr Fila <piofila2000@gmail.com>
2018-11-04 19:30:16 +0100

    Equipment can now be read from a file

--- lib/util.py
+++ lib/util.py
@@ -2,19 +2,33 @@ import math
 
 
 class Parser:
-    def __init__(self, path, separator='=', escape='"'):
+    def __init__(self, path, escape='"'):
         self.path = path
-        self.separator = separator
         self.escape = escape
         with open(path) as f:
             self.file = f.readlines()
 
     def readall(self):
+        ret = []
         for line in self.file:
-            pos = 0
-            for ch in line:
-                if ch == ' ' or ch == '\n':
-                    print('xd')
+            var = ''
+            temp = 0
+            inphr = True
+            for ch in range(1, len(line)):
+                if line[ch] == self.escape:
+                    if inphr:
+                        inphr = False
+                        if temp == 0:
+                            temp = var
+                        else:
+                            ret.append((temp, var))
+                            inphr = True
+                        var = ''
+                    else:
+                        inphr = True
+                elif inphr:
+                    var += line[ch]
+        return dict(ret)
 
 
 def dist(x1, y1, x2, y2):
